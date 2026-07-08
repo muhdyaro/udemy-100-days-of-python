4f75e9e2373966e4f1c6c1dad0e98f4d2637975b
Muhammad Yaro <muhdayaro@gmail.com>
2026-07-08 09:15:17 +0100

    Add Day 3, Day 4, Day 5, Day 6, Day 8, Day 18 notes and Python projects

---
+++ udemy-100-days-of-python/snake_game/main.py
@@ -0,0 +1,47 @@
+from turtle import Screen
+from snake import Snake
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    # Detect collision with wall
+    head = snake.segments[0]
+
+    if (
+        head.xcor() > 280
+        or head.xcor() < -280
+        or head.ycor() > 280
+        or head.ycor() < -280
+    ):
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
